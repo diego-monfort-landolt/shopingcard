974d0974dc0a3050412f278939cc4dd7479ad429
diego-monfort-landolt <diegomonfort1989@gmail.com>
2023-09-03 17:23:12 +0200

    update
    TODO- start the project
    shopping card

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+# Shopping Card
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+
+  return (
+    <>
+
+      <h1>Herzlich Wilkommen</h1>
+
+   
+
+    </>
+  )
+}
+
+export default App
